d2620f1363572a3fd806cff38c2d30e4e1183518
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-11-04 13:26:17 -0500

    nested loops

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 Isaac Ip All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Oct 2025
+ * This program uses nested loops.
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter1 = 0
+let loopCounter2 = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// when "A" is pressed, the pixels move cw
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    loopCounter1 = 0
+    while (loopCounter1 < 4) {
+        loopCounter1 = loopCounter1 + 1
+        loopCounter2 = 0
+        while (loopCounter2 < 4) {
+            sprite.move(1)
+            basic.pause(200)
+            loopCounter2 = loopCounter2 + 1
+        }
+        sprite.turn(Direction.Right, 90)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Yes)
+})
